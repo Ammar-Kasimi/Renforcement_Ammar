9a36c3969eab982402001a7c3489b38c74b15cd5
Your GitHub Username <your-github-email@example.com>
2026-03-18 15:26:50 +0000

    just created the repository and added the exercice from day one  (lundi),still need to update the structure for accessibility

---
+++ code.js
@@ -0,0 +1,135 @@
+
+const songs = [
+  {
+    titre: "Bohemian Rhapsody",
+    artiste: "Queen",
+    duree: 354,
+    genre: "Rock"
+  },
+  {
+    titre: "Billie Jean",
+    artiste: "Michael Jackson",
+    duree: 294,
+    genre: "Pop"
+  },
+  {
+    titre: "Shape of You",
+    artiste: "Ed Sheeran",
+    duree: 233,
+    genre: "Pop"
+  },
+  {
+    titre: "Hotel California",
+    artiste: "Eagles",
+    duree: 390,
+    genre: "Rock"
+  },
+  {
+    titre: "Smells Like Teen Spirit",
+    artiste: "Nirvana",
+    duree: 301,
+    genre: "Grunge"
+  },
+  {
+    titre: "Lose Yourself",
+    artiste: "Eminem",
+    duree: 326,
+    genre: "Rap"
+  },
+  {
+    titre: "Stairway to Heaven",
+    artiste: "Led Zeppelin",
+    duree: 482,
+    genre: "Rock"
+  },
+  {
+    titre: "Rolling in the Deep",
+    artiste: "Adele",
+    duree: 228,
+    genre: "Soul"
+  },
+  {
+    titre: "Blinding Lights",
+    artiste: "The Weeknd",
+    duree: 200,
+    genre: "Synthwave"
+  },
+  {
+    titre: "Get Lucky",
+    artiste: "Daft Punk",
+    duree: 249,
+    genre: "Electro"
+  }
+];
+
+
+// songs.forEach(e => {
+//     console.log(e.titre);
+// });
+
+
+
+// let rocks=[];
+// songs.forEach(e =>{
+//     if(e.genre=="Rock"){
+//             rocks.push(e)
+//              console.log(e.titre);
+//     }
+   
+// })
+
+// let Dsongs=[];
+// songs.forEach(e=>{
+//     let str=""
+//     str+=parseInt(e.duree/60)+":"+e.duree%60
+//     e.duree=str
+//     Dsongs.push(e)
+   
+    
+// })
+
+
+
+// let dureeTotale=0;
+// songs.forEach(e=>{
+// dureeTotale+=e.duree
+    
+// })
+// console.log(parseInt(dureeTotale/60)+":"+dureeTotale%60)
+
+
+
+// let max= songs[0];
+
+// songs.forEach(e=>{
+// if(max.duree<e.duree){
+//     max=e
+// }
+// })
+// console.log(max.titre);
+
+//skippit 6 o 7 7it nafs l7aja t9riban n3rf ndirhoum
+
+//hadi bla fonction predefini
+let arr=songs
+// let temp
+// for(let i=0;i<songs.length;i++){
+//     for(let j=i;j<songs.length;j++){
+//         if(arr[i].duree>arr[j].duree){
+//             temp=arr[i];
+//             arr[i]=arr[j]
+//             arr[j]=temp
+//             i=0
+//             j=0
+
+//         }
+//     }
+// }
+
+//hadi b fonction predefini
+
+// arr.sort((first,sec)=>first.duree-sec.duree)
+
+// arr.forEach(e => {
+//     console.log(e.titre);
+// });
